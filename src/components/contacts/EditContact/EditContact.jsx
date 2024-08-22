e42b63ebd237e84fd0463f0ab3a50d48f80552ac
Srirajkumar <ksrirajk@email.com>
2024-08-22 23:06:55 +0530

    Add, Edit & View Contact Pages HTML Setup

--- src/components/contacts/EditContact/EditContact.jsx
+++ src/components/contacts/EditContact/EditContact.jsx
@@ -1,9 +1,56 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 let EditContact = () => {
     return(
         <React.Fragment>
-            <h2>EditContact</h2>
+            <section className="add-contact">
+                <div className="container p-3">
+                    <div className="row">
+                        <div className="col">
+                            <p className="h4 text-primary fw-bold">Edit Contact</p>
+                            <p className="fst-italic">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae incidunt, nisi illo perspiciatis non mollitia. Repellat veniam debitis odio dolorem nemo obcaecati maxime, reprehenderit molestias facilis placeat voluptatum, voluptate illo.</p>
+                        </div>
+                    </div>
+                    <div className="row align-items-center">
+                        <div className="col-md-4">
+                            <form>
+                                <div className="mb-2">
+                                    <input type="text" className="form-control" placeholder="Name" />
+                                </div>
+                                <div className="mb-2">
+                                    <input type="text" className="form-control" placeholder="Photo URL" />
+                                </div>
+                                <div className="mb-2">
+                                    <input type="number" className="form-control" placeholder="Mobile" />
+                                </div>
+                                <div className="mb-2">
+                                    <input type="email" className="form-control" placeholder="Email" />
+                                </div>
+                                <div className="mb-2">
+                                    <input type="text" className="form-control" placeholder="Company Name" />
+                                </div>
+                                <div className="mb-2">
+                                    <input type="text" className="form-control" placeholder="Title" />
+                                </div>
+                                <div className="mb-2">
+                                    <select className="form-control">
+                                        <option value="">Select a Group</option>
+                                    </select>
+                                </div>
+                                <div className="mb-2">
+                                    <input type="submit" className="btn btn-primary" value="Update" />
+                                    <Link to={'/contacts/list'} className="btn btn-dark ms-2">Cancel</Link>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="col-md-6">
+                            <img src="https://cdn-icons-png.flaticon.com/512/219/219986.png" alt="" className="contact-img"/>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </React.Fragment>
     )
 };
--- src/components/contacts/ViewContact/ViewContact.jsx
+++ src/components/contacts/ViewContact/ViewContact.jsx
@@ -1,9 +1,56 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 let ViewContact = () => {
     return(
         <React.Fragment>
-            <h2>ViewContact</h2>
+            <section className="view-contact-intro p-3">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <p className="h3 text-warning fw-bold">View Contact</p>
+                            <p className="fst-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe beatae aliquam quaerat. Tenetur ipsam consequuntur, quidem eveniet voluptates voluptate, vel porro perspiciatis quo minima officia, provident aperiam reprehenderit. Ut, doloremque.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="view-contact mt-3">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-md-4">
+                            <img src="https://cdn-icons-png.flaticon.com/512/219/219986.png" alt="" className="contact-img" />
+                        </div>
+                        <div className="col-md-8">
+                            <ul className="list-group">
+                                <li className="list-group-item list-group-item-action">
+                                    Name : <span className="fw-bold">Raj</span>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    Mobile : <span className="fw-bold">9123456789</span>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    Email : <span className="fw-bold">raj@gmail.com</span>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    Company : <span className="fw-bold">RSA</span>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    Title : <span className="fw-bold">Frontend Developer</span>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    Group : <span className="fw-bold">O+ve</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="row mt-5">
+                        <div className="col">
+                            <Link to={'/contacts/list'} className="btn btn-warning">Back</Link>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </React.Fragment>
     )
 };
